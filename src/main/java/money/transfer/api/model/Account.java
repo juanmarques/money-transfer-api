e291c714b90fc69a79f2f1cac2db4e0feddfc44a
Juan Marques <1juanmarques@gmail.com>
2019-04-08 23:12:53 +0100

    Implement of hashCode

--- src/main/java/money/transfer/api/model/Account.java
+++ src/main/java/money/transfer/api/model/Account.java
@@ -1,5 +1,7 @@
 package money.transfer.api.model;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -54,5 +56,22 @@ public class Account {
 
 	public void setOwner(String owner) {
 		this.owner = owner;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		return id == other.id;
 	}
 }
